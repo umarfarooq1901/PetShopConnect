bd46e8f4640608b446e0de87804f5c4187a7cb47
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-17 15:08:51 +0530

    initialized the delete route

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,25 +1,29 @@
+const userSignupController = async (req, res) => {
+  try {
+    console.log("signing up");
+  } catch (error) {
+    console.log("Error while signing up the user!", error);
+  }
+};
 
+const userLoginController = async (req, res) => {
+  try {
+    console.log("login successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+const userDeleteController = async (req, res) => {
+  try {
+    console.log("delete successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-const userSignupController = async(req, res)=>{
-        try {
-            console.log('signing up')
-            
-        } catch (error) {
-            console.log('Error while signing up the user!',error);
-            
-        }
-}
-
-const userLoginController = async(req, res)=>{
-        try {
-            console.log('login successfully')
-        } catch (error) {
-            console.log(error);
-            
-        }
-}
-
-
-module.exports = {userSignupController, userLoginController}
+module.exports = {
+  userSignupController,
+  userLoginController,
+  userDeleteController,
+};
